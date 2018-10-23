c1a3966daf5a0a15f25c661ef9ca5e0b2a310283
Ben Mangold <benmangold@gmail.com>
2018-10-23 15:10:01 -0400

    styled components working, displaying test image with width 100%

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import styled from 'styled-components'
 
 const TEST_IMAGE_URL = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/t9QmCD4rQEmdqhiXUZPN_AI7B6379%20copy.jpg?auto=format&fm=jpg&fit=crop&w=800&h=242.42424242424244&bg=f0f0f0&q=38&dpr=2'
 class Gallery extends React.Component {
@@ -13,7 +14,11 @@ class Gallery extends React.Component {
   }
 
   render() {
-    return(<div><img src={TEST_IMAGE_URL} onClick={this.handleImageClick}></img></div>)
+    const Image = styled.img`
+      width: 100%
+    `
+
+    return(<div><Image src={TEST_IMAGE_URL} onClick={this.handleImageClick}></Image></div>)
   }
 }
 
