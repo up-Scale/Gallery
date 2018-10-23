bc1dfb914977abc2d0b3ce26e04fa9a8417ba1f7
Ben Mangold <benmangold@gmail.com>
2018-10-23 14:15:19 -0400

    added click handler to image tag. this will trigger the overlay in a future ticket

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -3,8 +3,17 @@ import ReactDom from 'react-dom';
 
 const TEST_IMAGE_URL = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/t9QmCD4rQEmdqhiXUZPN_AI7B6379%20copy.jpg?auto=format&fm=jpg&fit=crop&w=800&h=242.42424242424244&bg=f0f0f0&q=38&dpr=2'
 class Gallery extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleImageClick = this.handleImageClick.bind(this);
+  }
+
+  handleImageClick(event) {
+    console.log('you clicked the image!')
+  }
+
   render() {
-    return(<div><img src={TEST_IMAGE_URL}></img></div>)
+    return(<div><img src={TEST_IMAGE_URL} onClick={this.handleImageClick}></img></div>)
   }
 }
 
