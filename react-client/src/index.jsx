964a91ac58d8c80d9b81b72c0cfac3a4ded38761
Ben Mangold <benmangold@gmail.com>
2018-10-23 14:07:03 -0400

    added test image url and img tag to component

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const TEST_IMAGE_URL = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/t9QmCD4rQEmdqhiXUZPN_AI7B6379%20copy.jpg?auto=format&fm=jpg&fit=crop&w=800&h=242.42424242424244&bg=f0f0f0&q=38&dpr=2'
 class Gallery extends React.Component {
   render() {
-    return(<div>Hello World</div>)
+    return(<div><img src={TEST_IMAGE_URL}></img></div>)
   }
 }
 
