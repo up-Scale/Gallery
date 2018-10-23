21b6fa06529a9a1630f5750e19f5dbbd9fc7117d
Ben Mangold <benmangold@gmail.com>
2018-10-23 18:51:40 -0400

    serving static image data

--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ let app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
+
+const BANNER_IMG = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/t9QmCD4rQEmdqhiXUZPN_AI7B6379%20copy.jpg?auto=format&fm=jpg&fit=crop&w=800&h=242.42424242424244&bg=f0f0f0&q=38&dpr=2'
+
+app.get('/productImages', (req, res) => {
+  const dummyImages = { 
+    bannerImageUrl: BANNER_IMG,
+    images: [BANNER_IMG, BANNER_IMG, BANNER_IMG]
+  }
+
+  res.send(JSON.stringify(dummyImages))
+})
+
 app.listen(3000, () => {
   console.log('listening')
 })
