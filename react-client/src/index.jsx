43912b1116a80cdbb0adf944d8e145169aa0cc29
Ben Mangold <benmangold@gmail.com>
2018-10-24 19:56:26 -0400

    cleaning up parent cpt

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -23,17 +23,6 @@ ${Container}:hover & {
   opacity: 1;
 }
 `
-const Overlay = styled.div`
-height: 100%;
-width: 100%;
-display: ${props => props.overlay ? "block" : "none"};
-position: fixed;
-z-index: 1;
-top: 0;
-left: 0;
-background-color: rgb(0,0,0);
-background-color: rgba(0,0,0, 0.9);
-`
 const TEST_IMAGE_URL = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/t9QmCD4rQEmdqhiXUZPN_AI7B6379%20copy.jpg?auto=format&fm=jpg&fit=crop&w=800&h=242.42424242424244&bg=f0f0f0&q=38&dpr=2'
 
 const MAGNIFYING_GLASS_URL = 'https://image.flaticon.com/icons/svg/181/181561.svg'
@@ -52,10 +41,6 @@ class Gallery extends React.Component {
               onClick={this.handleImageClick}/>
             <MagnifyingGlass 
               src = {MAGNIFYING_GLASS_URL}/>
-            {/* <Overlay 
-              overlay={this.state.overlay}
-              onClick={this.handleOverlayClick}>
-            </Overlay> */}
             <GalleryOverlay
               overlay={this.state.overlay}
               handleClick={this.handleOverlayClick}
