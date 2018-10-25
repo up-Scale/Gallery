211bcdbdbf9f012764a2c430392e94ce6c75da46
Ben Mangold <benmangold@gmail.com>
2018-10-25 15:43:50 -0400

    documenting test json

---
+++ server/test.js
@@ -0,0 +1,15 @@
+// POST /productImages
+// post individual payloads to test db
+
+const testJsonPayloads = [
+{
+  "productName": "test",
+  "bannerImageUrl": "https://www.cats.org.uk/uploads/branches/1/42847%20Cats%20Weekly%20Lottery%20Web%20Banner.jpg",
+  "productImageUrls": ["https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg"]
+},
+{
+	"productName": "cats",
+	"bannerImageUrl": "https://www.cats.org.uk/uploads/branches/1/42847%20Cats%20Weekly%20Lottery%20Web%20Banner.jpg",
+	"productImageUrls": ["https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg"]
+},
+]
