384eabaa6c9041bfecee6822883db8359d06a079
Ben Mangold <benmangold@gmail.com>
2018-11-01 11:54:01 -0400

    filling in product image data

--- server/testJson.js
+++ server/testJson.js
@@ -1,40 +1,75 @@
 module.exports.giveMeFlashlight = (productName) => {
   return {
     "productName": productName,
-    "bannerImageUrl": "",
-	  "productImageUrls": ["", "", "", ""]
+    "bannerImageUrl": "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/PAHNdxJRHeuuutzQqqs7_banner.jpg?auto=format&fm=jpg&fit=crop&w=1300&h=393.93939393939394&bg=f0f0f0&q=38&dpr=2",
+    "productImageUrls": [
+    "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/ymi3GLTpRR3l674PzWHB_page1.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2", 
+    "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/N1jpcQzRTGunOpUNhE1J_qdQZJ0r.jpg?auto=format&fm=jpg&fit=crop&w=313&bg=f0f0f0&dpr=2", 
+    "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/JmR8zD6TG6zc88mQkW6x_ll0iIrKg.jpg?auto=format&fm=jpg&fit=crop&w=313&bg=f0f0f0&dpr=2", 
+    "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/5IeCUA3lQrq7pOJ2q1CJ_71a-JAdUymL._SL1280_.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+    "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/5i35oRiSxeDlJT2Jrs2n_watermark.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+    "https://massdrop-s3.imgix.net/product-images/nitecore-ec22-1000-lumen-infinite-led-flashlight/FP/c6icUJO7TbGowMXu3onU_TxTxLSa.jpg?auto=format&fm=jpg&fit=crop&w=313&bg=f0f0f0&dpr=2"
+  ]
   }
 }
 
 module.exports.giveMeHeadphones = (productName) => {
   return {
     "productName": productName,
-    "bannerImageUrl": "",
-	  "productImageUrls": ["", "", "", ""]
+    "bannerImageUrl": "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/oPTmYvneR4mgwCXxVo8m_AI7B2574-copy-banner.jpg?auto=format&fm=jpg&fit=crop&w=1600&h=484.8484848484849&bg=f0f0f0&q=38&dpr=2",
+    "productImageUrls": [
+      "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/XbUb57ecT9ydionwPeP5_AI7B2076-copy.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/MWs9iF0DTYekbci9Klb1_AI7B9691-copy.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/ZDXG4jirTL2QZjZKxa4X_AI7B9701-copy.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/oamINBMRTyiBVdaVjCry_AI7B9685-copy.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2",
+      "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/22WXXOstQNKjYnIb9mOG_AI7B9703-copy.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/massdrop-x-hifiman-edition-xx-headphones/FP/n4qH0V56QVeSmO9BQzBL_AI7B9737-copy.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2"
+    ]
   }
 }
 
 module.exports.giveMeKeyboard = (productName) => {
   return {
     "productName": productName,
-    "bannerImageUrl": "",
-	  "productImageUrls": ["", "", "", ""]
+    "bannerImageUrl": "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/Wle56GwdRgWPrbNaWRR7_AI7B6090-copy-banner.jpg?auto=format&fm=jpg&fit=crop&w=1600&h=484.8484848484849&bg=f0f0f0&q=38&dpr=2",
+	  "productImageUrls": [
+      "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/IhT3NhxzSAONgRhKbY7a_AI7B5534-copy.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/cxWI1GyoSGGch6osWQrP_AI7B6777-copy.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/ZOr1IE4RxuDmTVjv0gsJ_AI7B6804-copy.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/XbWiNmBkQPqwVhk8EEMa_AI7B5416-copy-checkout1.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2",
+      "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/Ew9RSUodQaCBRJ3KL1xM_AI7B5416-copy-checkout2.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2",
+      "https://massdrop-s3.imgix.net/product-images/planck-mechanical-keyboard/FP/F9Q4Ma2YR0WVVKlLbu3C_AI7B5192-copy-pc-dark.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2"
+    ]
   }
 }
 
 module.exports.giveMeSeki = (productName) => {
   return {
     "productName": productName,
-    "bannerImageUrl": "",
-	  "productImageUrls": ["", "", "", ""]
+    "bannerImageUrl": "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-26696_20160907100117_7a4ec8e1df101933.jpg?auto=format&fm=jpg&fit=crop&w=1300&h=393.93939393939394&bg=f0f0f0&q=38&dpr=2",
+	  "productImageUrls": [
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140124_2b713cd4c2a512bf.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140116_ae1e49ea4ed70295.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140120_9530bb0e7d7210ed.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140119_6ae07590600c9eb1.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140121_cf0b2f9b40987d9c.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140122_55bc1e6f8abdab8e.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2",
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140121_6822ac4fc567f904.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2",
+      "https://massdrop-s3.imgix.net/product-images/seki-edge-grooming-kit/MD-22295_20160630140121_cf0b2f9b40987d9c.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2"
+    ]
   }
 }
 
 module.exports.giveMeWatches = (productName) => {
   return {
     "productName": productName,
-    "bannerImageUrl": "",
-	  "productImageUrls": ["", "", "", ""]
+    "bannerImageUrl": "https://massdrop-s3.imgix.net/product-images/glycine-combat-7-vintage-automatic-watch/MD-35241_20170307133651_61d31bad1fd919f0.jpg?auto=format&fm=jpg&fit=crop&w=1300&h=393.93939393939394&bg=f0f0f0&q=38&dpr=2",
+	  "productImageUrls": [
+      "https://massdrop-s3.imgix.net/product-images/glycine-combat-7-vintage-automatic-watch/MD-35241_20170314144456_2fce6411a07a1e77.jpg?auto=format&fm=jpg&fit=crop&w=955&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/glycine-combat-7-vintage-automatic-watch/MD-35241_20170310104243_76ad41890b54249c.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/glycine-combat-7-vintage-automatic-watch/MD-35241_20170310104242_81fcf644ff22679b.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2", 
+      "https://massdrop-s3.imgix.net/product-images/glycine-combat-7-vintage-automatic-watch/MD-35241_20170310104242_3cbacbb3a05d7eb5.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2"
+    ]
   }
 }
 
@@ -42,14 +77,22 @@ module.exports.giveMeCats = (productName) => {
   return {
     "productName": productName,
     "bannerImageUrl": "https://www.cats.org.uk/uploads/branches/1/42847%20Cats%20Weekly%20Lottery%20Web%20Banner.jpg",
-	  "productImageUrls": ["https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg", 'https://images.pexels.com/photos/74177/cat-74177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', "https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg", 'https://images.pexels.com/photos/74177/cat-74177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', "https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg", 'https://images.pexels.com/photos/74177/cat-74177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260']
+	  "productImageUrls": [
+      "https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg", 
+      'https://images.pexels.com/photos/74177/cat-74177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 
+      "https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg", 
+      'https://images.pexels.com/photos/74177/cat-74177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 
+      "https://timedotcom.files.wordpress.com/2015/02/grumpy-cat1.jpg", 
+      'https://images.pexels.com/photos/74177/cat-74177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    ]
   }
 }
 
 module.exports.giveMeDogs = (productName) => {
   return {
     "productName": productName,
-    "bannerImageUrl": "",
-	  "productImageUrls": ["", "", "", ""]
+    "bannerImageUrl": "https://www.petconnectrescue.org/wp-content/uploads/2013/05/three-dogs-banner-e1500758981792.jpg",
+	  "productImageUrls": [
+    ]
   }
 }
