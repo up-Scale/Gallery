ab6ea06becf4cace1c7b51305cfe3b49d3b026a7
Ben Mangold <benmangold@gmail.com>
2018-11-01 16:11:55 -0400

    no more static content at all from server

--- server/index.js
+++ server/index.js
@@ -10,9 +10,6 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.use(parser.json());
 
-const BANNER_IMG = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/t9QmCD4rQEmdqhiXUZPN_AI7B6379%20copy.jpg?auto=format&fm=jpg&fit=crop&w=800&h=242.42424242424244&bg=f0f0f0&q=38&dpr=2'
-const CAROUSEL_IMG_URL = 'https://massdrop-s3.imgix.net/product-images/massdrop-x-sennheiser-hd-58x-jubilee-headphones/FP/UbUHmV3QPiZTK3nHpAHJ_361A2108.jpg?auto=format&fm=jpg&fit=crop&w=473&bg=f0f0f0&dpr=2'
-
 app.get('/buy/:productName', (req, res) => {
   res.sendFile(path.resolve('react-client/dist/index.html'))
 })
