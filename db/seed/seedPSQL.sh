cc9176e04ca58131c8cd63892ce47da1a8bf4264
Stacy Kim <Stacy@Stacys-MacBook-Air-92693.local>
2018-11-27 11:53:13 -0500

    Fixed PostgreSQL bash script to make ID the primary key

--- db/seed/seedPSQL.sh
+++ db/seed/seedPSQL.sh
@@ -5,3 +5,5 @@ psql -h localhost -d postgres -p 5432 -a -q -f db/schemas/schemaPSQL.sql
 time psql -d postgres -c "\copy \"temp\" FROM 'db/products.csv' delimiter ',' csv header"
 time psql -d postgres -c "INSERT INTO products SELECT * FROM temp ON CONFLICT DO NOTHING"
 time psql -d postgres -c "ALTER TABLE products ADD id SERIAL"
+psql -d postgres -c "ALTER TABLE products DROP CONSTRAINT products_pkey;"
+psql -d postgres -c "ALTER TABLE products ADD PRIMARY KEY (id);"
